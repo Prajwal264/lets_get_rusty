d2061c5334e6f77be5b0cd2b0b0ed70cee6fb2de
Prajwal <prajwal@192.168.1.21>
2023-12-03 14:47:22 +0530

    feat: adding a simple server starter

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,34 @@
-use std::sync::Mutex;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
 
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
-
-// Create a GET request
-#[get("/hello")]
-// Create a function that will be involked when the above mentioned route is hit.
-async fn hello() -> impl Responder {
-    // Response with Ok and a message body
-    HttpResponse::Ok().body("Hello World")
+#[derive(Serialize)]
+pub struct Response {
+    pub message: String,
 }
 
-struct AppStateWithCounter {
-    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+#[get("/health")]
+async fn healthcheck() -> impl Responder {
+    let response = Response {
+        message: "Everything is working fine".to_string(),
+    };
+    HttpResponse::Ok().json(response)
 }
 
-async fn counter_route_handler(data: web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
-    *counter += 1; // <- access counter inside MutexGuard
-    format!("Request number: {counter}") // <- response with count
+async fn not_found() -> impl Responder {
+    let response = Response {
+        message: "Resource not found".to_string(),
+    };
+    HttpResponse::Ok().json(response)
 }
 
-/// extract path info from "/users/{user_id}/{friend}" url
-/// {user_id} - deserializes to a u32
-/// {friend} - deserializes to a String
-#[get("/users/{user_id}/{friend}")] // <- define path parameters
-async fn path_extraction_handler(path: web::Path<(u32, String)>) -> Result<String> {
-    let (user_id, friend) = path.into_inner();
-    Ok(format!("Welcome {}, user_id {}!", friend, user_id))
-}
-
-// Called first
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Note: web::Data created _outside_ HttpServer::new closure
-    let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
-    });
-    // Create a new Http Server
-    HttpServer::new(move || {
-        // Create an Application
+    HttpServer::new(|| {
         App::new()
-            .app_data(counter.clone())
-            .route("/counter", web::get().to(counter_route_handler))
-            .service(hello)
-            .service(path_extraction_handler)
+            .service(healthcheck)
+            .default_service(web::route().to(not_found))
     })
-    // Bind the server to a port
     .bind(("127.0.0.1", 8080))?
-    // run the application
     .run()
-    // Suspend the execution until the future is ready
     .await
 }
